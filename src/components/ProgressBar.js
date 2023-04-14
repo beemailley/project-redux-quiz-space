bed08ddc6aee4d7c800ae7baea752df7d4dc2c9a
Bridget Mailley <bmailley42@gmail.com>
2023-04-14 15:27:02 +0200

    scroll to top of summary

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 export const ProgressBar = () => {
   const amountOfQuestions = useSelector((state) => state.quiz.questions.length);
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
   const answers = useSelector((state) => state.quiz.answers)
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <div className="progress-bar">
